4fa63316ce5b544d0d3f93069fa658c78ba361c8
tmxk2021 <90445826+tmxk2021@users.noreply.github.com>
2025-02-22 15:48:37 +0800

    Update openwrtlive.sh

--- openwrtlive.sh
+++ openwrtlive.sh
@@ -61,7 +61,7 @@ else
 fi
 
 # 下载并替换Nginx配置文件（强制替换）
-NGINX_CONF_URL="https://raw.githubusercontent.com/rad168/iptv/refs/heads/main/mytv/nginx.conf"
+NGINX_CONF_URL="https://raw.githubusercontent.com/tmxk2021/CF-IPTV/refs/heads/main/nginx.conf"
 NGINX_CONF_PATH="/etc/nginx/nginx.conf"
 echo "下载并强制替换Nginx配置文件..."
 curl -o "$NGINX_CONF_PATH" "$NGINX_CONF_URL"
